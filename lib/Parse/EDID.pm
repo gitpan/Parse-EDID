5a1c6471d547b00739e7e858b338f3878fe33d57
Guillaume Rousse <grousse@cpan.org>
2013-12-24 08:28:07 +0000

    Import of GROUSSE/Parse-EDID-1.0.6 from CPAN.
    gitpan-cpan-distribution: Parse-EDID
    gitpan-cpan-version:      1.0.6
    gitpan-cpan-path:         GROUSSE/Parse-EDID-1.0.6.tar.gz
    gitpan-cpan-author:       GROUSSE
    gitpan-cpan-maturity:     released

--- lib/Parse/EDID.pm
+++ lib/Parse/EDID.pm
@@ -21,7 +21,7 @@ use strict;
 use warnings;
 use base 'Exporter';
 
-our $VERSION = '1.0.5';
+our $VERSION = '1.0.6';
 
 our @EXPORT = qw(
      parse_edid
@@ -674,3 +674,18 @@ empty string otherwise.
 
 Extract raw binary edid data from another tool text output, such as xrandr
 --prop.
+
+=head1 LICENSE
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
